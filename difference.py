bcc13fbd0789bfe1c0ba90e968b09828a50a22c4
Braeden Schaefer <braedensschaefer@gmail.com>
2018-02-11 18:16:15 -0700

    Sum
    Changed from difference to sum, and fixed an error in my code that prevented the function from working properly. I had originally told the function to print "r" when I meant to have it print "d".

--- difference.py
+++ difference.py
@@ -1,12 +1,10 @@
-def difference(x, y):
+def sum(x, y):
    # number 1: x
    # number 2: y
-   "calulate the difference between two numbers"
-   return (x-y)
+   "calulate the sum of two numbers"
+   return (x+y)
 
-d = difference(1584,732)
-print(r)
-if r < 0:
-   print("If you were looking for a positive value, try switching x and y")
+d = sum(1584,732)
+print(d)
 
    
