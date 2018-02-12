43e08fe23a5e99591f1627ca1c9ad982425e07d6
Braeden Schaefer <braedensschaefer@gmail.com>
2018-02-11 18:13:49 -0700

    Difference
    calculate the difference between two numbers

---
+++ difference.py
@@ -0,0 +1,12 @@
+def difference(x, y):
+   # number 1: x
+   # number 2: y
+   "calulate the difference between two numbers"
+   return (x-y)
+
+d = difference(1584,732)
+print(r)
+if r < 0:
+   print("If you were looking for a positive value, try switching x and y")
+
+   
